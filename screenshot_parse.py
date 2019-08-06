5c73a1ab0ab29efa4e4d39f9ca1e11f9168523fb
squaresmile <squaresmile@protonmail.com>
2019-08-06 10:04:57 -0400

    New templates for sumer 2 part 2

--- screenshot_parse.py
+++ screenshot_parse.py
@@ -1,8 +1,12 @@
+import time
 import argparse
 import os
 import pprint
 import cv2
+import numpy as np
 import pytesseract
+
+# from scipy import stats
 
 OUTPUT_FILE = "parsed_hp.csv"
 APOC_BOSS_TEMPLATES_FOLDER = "templates/cn-apocrypha/bosses"
@@ -14,30 +18,47 @@ def get_numbers_from_text(text):
     return "".join(digits)
 
 
-def parse_hp(image, debug=False):
+def parse_hp(image, debug=False, channel_sameness_check=True):
     if debug:
         cv2.imwrite("1 cropped.png", image)
 
-    # hsv = cv2.cvtColor(cropped, cv2.COLOR_BGR2HSV)
-    # It's easier to filter out white with BGR
-    lower_color = (155, 155, 155)
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2HLS)
+    sensitivity = 150
+    lower_color = (255 - sensitivity, 255 - sensitivity, 255 - sensitivity)
     upper_color = (255, 255, 255)
-    mask = cv2.inRange(image, lower_color, upper_color)
-    # if debug:
-    #     cv2.imwrite("2 mask.png", mask)
-    # filtered = cv2.bitwise_and(cropped, cropped, mask=mask)
-    # if debug:
-    #     cv2.imwrite("3 filtered.png", filtered)
-
-    # gray = cv2.cvtColor(filtered, cv2.COLOR_BGR2GRAY)
-    # if debug:
-    #     cv2.imwrite("4 gray.png", gray)
-    # _, thres = cv2.threshold(gray, 65, 255, cv2.THRESH_BINARY_INV)
-    thres = cv2.bitwise_not(mask)
+    thres = cv2.inRange(image, lower_color, upper_color)
+    thres = cv2.bitwise_not(thres)
     if debug:
-        cv2.imwrite("5 thres.png", thres)
+        cv2.imwrite("2 thres.png", thres)
+    percent_black = np.sum(thres == 0) / thres.size
+    if debug:
+        print(f"Percent black: {percent_black:.5f}")
+    if percent_black > 0.2 and channel_sameness_check:
+        colors = {i: image[:, :, i] for i in range(3)}
+        channel_sameness = 10
+        max_addition = 255 - channel_sameness
+        for color_x, img_color in colors.items():
+            # Without the maximum and minium, the addition and subtraction wraps around 0 and 255
+            lower_x = np.maximum(img_color, channel_sameness) - channel_sameness
+            upper_x = np.minimum(img_color, max_addition) + channel_sameness
+            color_y, color_z = [c for c in colors if c != color_x]
+            # If other channels y, z are not within +-sameness of x, the pixel get set to white
+            # Other light colors have different varying B, G, R values while FGO whites are pretty close
+            idx = (
+                (colors[color_y] < lower_x)
+                | (colors[color_y] > upper_x)
+                | (colors[color_z] < lower_x)
+                | (colors[color_z] > upper_x)
+            )
+            thres[idx] = 255
+        if debug:
+            cv2.imwrite("3 thres same rgb values.png", thres)
 
-    ocr_text = pytesseract.image_to_string(thres, config='-l eng --oem 1 --psm 7')
+    #     _, labels = cv2.connectedComponents(thres, connectivity=4)
+    #     most_common_component = stats.mode(np.concatenate(labels))[0][0]
+    #     thres[labels == most_common_component] = 0
+
+    ocr_text = pytesseract.image_to_string(thres, config="-l eng --oem 1 --psm 7")
     if debug:
         print(f"raw Tesseract text: {ocr_text}")
     ocr_text = get_numbers_from_text(ocr_text)
@@ -82,7 +103,7 @@ def parse_apocrypha(image, debug=False):
         raise Exception(f"OpenCV can't read {image}")
     battles = {}
     for i in range(1, 4):
-        battle = image[31 + (i - 1) * 139:169 + (i - 1) * 139, 2:378]
+        battle = image[31 + (i - 1) * 139 : 169 + (i - 1) * 139, 2:378]
         battles[i] = battle
         if debug:
             cv2.imwrite(f"battle {i}.png", battle)
@@ -100,7 +121,9 @@ def parse_summer_race(image, debug=False):
     image = cv2.imread(image)
     if image is None:
         return [{"name": "", "hp": ""}]
-    teams_list = [t for t in os.listdir(SUMMER_RACE_TEAMS_TEMPLATE) if t.endswith(".png")]
+    teams_list = [
+        t for t in os.listdir(SUMMER_RACE_TEAMS_TEMPLATE) if t.lower().endswith(".png")
+    ]
     teams = {}
     for team_template in teams_list:
         team_img = cv2.imread(os.path.join(SUMMER_RACE_TEAMS_TEMPLATE, team_template))
@@ -112,7 +135,7 @@ def parse_summer_race(image, debug=False):
         res = cv2.matchTemplate(image, team_img, cv2.TM_CCOEFF_NORMED)
         _, max_val, _, max_loc = cv2.minMaxLoc(res)
         if debug:
-            print(f"Team: {team}, Matching value: {max_val}")
+            print(f"Team: {team}, Matching value: {max_val:.5f}")
         if max_val > 0.8:
             left, top = max_loc
             h, w = team_img.shape[:2]
@@ -122,7 +145,12 @@ def parse_summer_race(image, debug=False):
             team_split = team.split("_")
             add_bottom = int(team_split[1])
             team_name = team_split[0]
-            hp_img = image[min(bottom+add_bottom, max_height):min(bottom+add_bottom+50, max_height), max(left-100, 0):right]
+            hp_img = image[
+                min(bottom + add_bottom, max_height) : min(
+                    bottom + add_bottom + 50, max_height
+                ),
+                max(left - 150, 0) : right,
+            ]
             team_hp = parse_hp(hp_img, debug)
             output.append({"name": team_name, "hp": team_hp})
     return output
@@ -136,10 +164,15 @@ def parse_onigashima(image, debug=False):
 
 
 if __name__ == "__main__":
+    start = time.time()
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("-i", "--image", required=True, help="Input image")
-    arg_parser.add_argument("-d", "--debug", action='store_true', help="Write debug images")
+    arg_parser.add_argument(
+        "-d", "--debug", action="store_true", help="Write debug images"
+    )
     args = arg_parser.parse_args()
     result = parse_summer_race(args.image, args.debug)
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(result)
+    end = time.time()
+    print(f"Finished in {end - start:.5f}s")
